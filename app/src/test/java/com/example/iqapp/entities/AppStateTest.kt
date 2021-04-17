da866851562443ef43289d6e5a0d663e0b1dbfae
Nikolai Sviridov <nikolaisvg@gmail.com>
2021-04-17 15:22:52 +0300

    Add test

--- app/src/test/java/com/example/iqapp/entities/AppStateTest.kt
+++ app/src/test/java/com/example/iqapp/entities/AppStateTest.kt
@@ -2,19 +2,29 @@ package com.example.iqapp.entities
 
 import android.content.res.Resources
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.mock
 
 class AppStateTest {
-    @Test
-    fun `AppState loadTasks amount`() {
-        val numOfTasks = 10
+    private val numOfTasks = 10
 
+    @Before
+    fun setUp() {
         val resources = mock(Resources::class.java)
         AppState.loadTasks(numOfTasks, resources, null)
+    }
 
+    @Test
+    fun `AppState loadTasks amount`() {
         assertEquals(numOfTasks, AppState.tasks.size)
         assertEquals(numOfTasks, AppState.chosenAnswers.size)
         assertEquals(numOfTasks, AppState.shownAnswers.size)
+    }
+
+    @Test
+    fun `AppState passed tasks`() {
+        assertEquals(numOfTasks, AppState.passedTasks())
+        assertEquals(1.0, AppState.result(), 0.0001)
     }
 }
